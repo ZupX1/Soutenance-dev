4a9fb3dc8cbcd05960019457ee3d3f9e98641211
ZupX1 <vprevostcarpentier@normandiewebschool.fr>
2022-03-22 22:28:45 +0100

    fin du popup

---
+++ src/verif.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Les arbres remarquables</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='css/verif.css'>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+<script>
+	$(document).ready(function(){
+		$("#myModal").modal('show');
+	});
+</script>
+</head>
+<body>
+<div id="myModal" class="modal fade">
+    <div class="modal-dialog">
+        <div class="logo">
+            <img src="img/logo.png">
+        </div>
+        <div class="modal-contenu">
+                <h1 class="age">Avez-vous 18 ans ?<h1>
+				<p class="txt">Vous risquez de voir des images interdites aux mineurs...</p>
+                <form>
+                    <a class="button" data-controls-modal="modal-contenu" data-backdrop="static" data-keyboard="false" href="index.php">J'ai 18 ans ou plus - Entrer</a>
+                </form>
+        </div>
+    </div>
+</div>
+</body>
+</html>
