225559762f569022bdf12516a7abb43d3d2250ea
ZupX1 <vprevostcarpentier@normandiewebschool.fr>
2022-03-24 17:01:37 +0100

    add popup

--- src/Home.php
+++ src/Home.php
@@ -5,10 +5,28 @@
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <title>Les arbres remarquables</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel='stylesheet' type='text/css' media='screen' href='css/main.css'>
+    <link rel='stylesheet' type='text/css' media='screen' href='css/home.css'>
 </head>
-    <body>
-    <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-        <script src='js/main.js'></script>
-    </body>
-</html>
+<header>
+    <div class="logo">
+    <a href="home.php"><img src="img/logo.png"></a>
+    </div>
+</header>
+<body>
+    <div class="pub1">
+        <a href="home.php" onclick="window.open('popup.php');"><img src="img/doggy.jpg" class="stretch"> </a>
+    </div>
+    <p class="txtpub1"><a href="home.php" onclick="window.open('popup.php');">Rencontré des arbre chaud cyprès de chez toi</p></a>
+    <div class="videomap">
+    <a href="index.php" onclick="window.open('popup.php');"><img src="img/carte_paris.png" class="videomap1"></a>
+    </div>
+    <div class="play">
+        <a href="index.php" onclick="window.open('popup.php');"><img src="img/play.png" class="play"></a>
+    </div>
+    <div class="pub2">
+        <a href="home.php" onclick="window.open('popup.php');"><img src="img/peni.jpg"></a>
+        <p class="txtpub2"><a href="home.php" onclick="window.open('popup.php');">Veut-tu agrandir la taile de ton tronc ?</a></p>
+    </div>
+    <div class="txthori">
+    </div>
+</body>
---
+++ src/verif.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Les arbres remarquables</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='css/verif.css'>
+</head>
+<body>
+    <div class="container">
+        <div>
+            <img src="img/logo.png" class="photo">
+            <p class="age">Avez-vous 18 ans ?<p>
+	        <p class="txt">Vous risquez de voir des images interdites aux mineurs...</p>
+            <a href="home.php"><button class="button">J'ai 18 ans ou plus - Entrer</button></a>
+        </div>
+    </div>
+</body>
+</html>
+
+
